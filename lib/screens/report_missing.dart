d9704538d1e328720b7d788151936d4cbe9502c8
onio-rgb <astitvastu@gmail.com>
2022-05-25 05:03:29 +0530

    setup file upload and picker

--- lib/screens/report_missing.dart
+++ lib/screens/report_missing.dart
@@ -1,9 +1,13 @@
 import 'dart:ffi';
 import 'dart:io';
 
+import 'package:async_button_builder/async_button_builder.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:missing/custom%20widgets/textfield.dart';
+import 'package:missing/globals.dart';
 
 class ReportMissing extends StatefulWidget {
   const ReportMissing({Key? key}) : super(key: key);
@@ -13,7 +17,7 @@ class ReportMissing extends StatefulWidget {
 }
 
 class _ReportMissingState extends State<ReportMissing> {
-  late File? _image = null;
+  File? _image;
   //function to pick photo from either gallery or camera
 
   Future getImage(bool gallery) async {
@@ -37,6 +41,7 @@ class _ReportMissingState extends State<ReportMissing> {
     setState(() {
       if (pickedFile != null) {
         _image = File(pickedFile.path);
+        print("${_image!.path} Astitba");
       } else {
         _image = null;
         print('No image selected.');
@@ -44,6 +49,32 @@ class _ReportMissingState extends State<ReportMissing> {
     });
   }
 
+  Future<String> uploadFile(File image) async {
+    try {
+      final storageRef =
+          FirebaseStorage.instance.ref('missing/${image.path.split('/').last}');
+
+      await storageRef.putFile(image).then((p0) {
+        print('File Uploaded');
+      });
+
+      String returnURL = await storageRef.getDownloadURL();
+      return returnURL;
+    } on Exception catch (e) {
+      print("${e} Very");
+    }
+    return "";
+  }
+
+  Future<void> saveImages(DocumentReference ref) async {
+    // print("bery");
+    String imageURL = await uploadFile(_image!);
+    print("bery");
+    print("${imageURL} Astitba ");
+    ref.update({"image": imageURL});
+  }
+
+  final db = FirebaseFirestore.instance;
   late TextEditingController name = TextEditingController();
   late TextEditingController age = TextEditingController();
   late TextEditingController lastwear = TextEditingController();
@@ -129,7 +160,42 @@ class _ReportMissingState extends State<ReportMissing> {
                         height: double.infinity,
                       )
                     : Container()),
-          )
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: AsyncButtonBuilder(
+                  onPressed: () async {
+                    final details = {
+                      'name': name.text,
+                      'age': age.text,
+                      'lastwear': lastwear.text,
+                      'feet': int.parse(feet.text),
+                      'inches': int.parse(inches.text)
+                    };
+                    DocumentReference doc_ref = await db
+                        .collection('users')
+                        .doc(currentUid)
+                        .collection('missing people')
+                        .add(details);
+                    // print("bery");
+                    await saveImages(doc_ref);
+                  },
+                  child: Text(
+                    'Submit',
+                    style: TextStyle(fontSize: 17),
+                  ),
+                  builder: (context, child, callback, _) {
+                    return ElevatedButton(onPressed: callback, child: child);
+                  },
+                ),
+              ),
+              decoration:
+                  BoxDecoration(borderRadius: BorderRadius.circular(30)),
+            ),
+          ),
         ],
       ),
     );
